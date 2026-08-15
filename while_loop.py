b2ae7b0b3412b3b439c9cd32721263069e63ec42
Aditi Srivastava <aditiarvinds@gmail.com>
2026-08-15 17:53:37 +0530

    while loop in python

--- while_loop.py
+++ while_loop.py
@@ -2,3 +2,15 @@ i=1
 while i<=5:
     print("aditi")
     i+=1 #if we forget to write this then infinite loop is craeed as value of i will always remain 1 and as 1<5 always, loop will never end
+
+
+
+i=1
+while i<=5:
+    print(i* "*")
+    i+=1
+
+i=5
+while i>=1:
+    print(i*"*")
+    i-=1
