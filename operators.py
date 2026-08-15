7ab4f200aa861eb8c832293d857fd99a70d132f7
Aditi Srivastava <aditiarvinds@gmail.com>
2026-08-15 16:50:28 +0530

    operators in python

---
+++ operators.py
@@ -0,0 +1,22 @@
+print(2+3)
+print(2-3)
+print(2*3)
+print(2/3)
+print(2//3)
+print(2%3)
+print(2**3)
+
+#operator precedence
+print(2+3*3)
+print((2+3)*3)
+
+print(3>2)
+print(1<2)
+print(3<2)
+print(3!=2)
+
+print((3>2)or(3<4))
+print((3>2)and(3<4))
+print(not((3>2)or(3<4)))
+
+
