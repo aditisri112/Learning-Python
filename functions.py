a223375caa4e061a6c3bfe47831e04a8b12ba02d
Aditi Srivastava <aditiarvinds@gmail.com>
2026-08-15 22:36:55 +0530

    functions in python

---
+++ functions.py
@@ -0,0 +1,18 @@
+def sum(a,b):
+    summation=a+b
+    print(summation)
+
+sum(1,2)
+
+print(len([1,2,3,4,5]))
+print(min([1,2,3,4,5]))
+print(max([1,2,3,4,5]))
+print(type([1,2,3,4,5]))
+
+from math import sqrt, log2
+print(sqrt(16))
+print(log2(16))
+
+import random
+print(random.random())
+print(random.randint(1,10))
