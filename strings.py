141595bad8fa8730fb8f91c9ef77859c8b5bea4d
Aditi Srivastava <aditiarvinds@gmail.com>
2026-08-15 16:19:42 +0530

    string operations in python

---
+++ strings.py
@@ -0,0 +1,14 @@
+name="Tony Stark"
+
+print(name.upper())
+
+print(name.lower())
+
+print(name.find("k"))
+print(name.find("e"))
+
+print(name.replace("Stark", "aditi"))
+print(name.replace("T", "Ph"))
+
+print('X' in name)
+print('T' in name)
