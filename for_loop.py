d7399403b090c7bde1b1e84b969a5a87cf1f4e74
Aditi Srivastava <aditiarvinds@gmail.com>
2026-08-15 17:57:18 +0530

    for loops in python

---
+++ for_loop.py
@@ -0,0 +1,17 @@
+for i in range(5):
+    print(i)
+
+for i in range(1, 6):
+    print(i)
+
+for i in range(1, 51):
+    if i==21:
+        break
+    if i%3==0:
+        print(i)
+
+for i in range(1, 51):
+    if i==21:
+        continue
+    if i%3==0:
+        print(i)
